18591d79aa33f321ed50f3499f9005513b2305aa
Eugene Pavlovets <y.pavlovets@pixelplex.io>
2019-12-07 13:08:18 +0300

    [SDC-116] Changed connection sidebar icons

--- src/containers/Layout/Sidebar/index.jsx
+++ src/containers/Layout/Sidebar/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import history from '../../../history';
+import { WalletIcon, ReceiveIcon, SettingsIcon, SendIcon } from './../../../constants/SidebarIcon';
 
 import {
 	DASHBOARD_PATH,
@@ -13,22 +14,24 @@ const Sidebar = () => {
 
 	const isActive = (path) => (history.location.pathname === path ? 'is-active' : '');
 
+	console.log(ReceiveIcon);
+
 	return (
 		<div className="sidebar">
 			<NavLink to={DASHBOARD_PATH} className={`sidebar-item ${isActive(DASHBOARD_PATH)}`}>
-				<i className="wallet-icon is-icon" />
+				<i className="is-icon  wallet-icon">{WalletIcon}</i>
 				<p>Wallet</p>
 			</NavLink>
 			<NavLink to={SEND_TRANSACTION_PATH} className={`sidebar-item ${isActive(SEND_TRANSACTION_PATH)}`}>
-				<i className="send-icon is-icon" />
+				<i className="is-icon">{SendIcon}</i>
 				<p>Send</p>
 			</NavLink>
 			<NavLink to={RECEIVE_PATH} className={`sidebar-item ${isActive(RECEIVE_PATH)}`}>
-				<i className="receive-icon is-icon" />
+				<i className="is-icon">{ReceiveIcon}</i>
 				<p>Receive</p>
 			</NavLink>
 			<NavLink to={SETTINGS_PATH} className={`sidebar-item ${isActive(SETTINGS_PATH)}`}>
-				<i className="settings-icon is-icon" />
+				<i className="is-icon">{SettingsIcon}</i>
 				<p>Settings</p>
 			</NavLink>
 		</div>
