7c1784f23be22301edb86023174a431935a29d92
Eugene Pavlovets <y.pavlovets@pixelplex.io>
2019-12-09 18:05:20 +0300

    [SDC-101] Added application icons and splash screen

--- src/components/SplashScreen/index.jsx
+++ src/components/SplashScreen/index.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 
 const SplashScreen = ({ title, children }) => (
 	<div className="authorization-page">
-		<div className="logo" />
 		<div className="authorization-page__title">{title}</div>
 		<div className="authorization-page__bottom-container">
 			{children}
